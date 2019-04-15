ea89e3a031718e4e60300796c60a9e057466bf57
kaushilk <kkash321@gmail.com>
2019-04-15 16:14:27 -0400

    master

--- kashblg/app/kashblg.py
+++ kashblg/app/kashblg.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+@app.route('/resume/')
+def resume():
+    return render_template('resume.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
